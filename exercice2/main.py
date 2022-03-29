c8d74f497619e6df73d4783c31f9e3687f58437c
Octave-L <octave.laurans@gmail.com>
2022-03-29 18:43:00 +0200

    Integration exo5

--- exercice2/main.py
+++ exercice2/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from book import Book
 def main():
+    print("\nExercise 2 and 3\n")
     book = Book("TEST")
     book.insert_buy(10, 10.0)
     book.insert_sell(120, 12.0)
@@ -8,8 +9,9 @@ def main():
     book.insert_buy(2, 11.0)
     book.insert_sell(1, 10.0)
     book.insert_sell(10, 10.0)
-    book.visualisationPandas()
 
+    print("\nExercise 5")
+    book.visualisationPandas()
 
 if __name__ == "__main__":
     main()
